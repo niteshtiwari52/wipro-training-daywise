c461322bcd797b0d4ec949c7280686efd97d8275
niteshtiwari <nitesh.tiwari.coox@gmail.com>
2025-08-08 21:52:03 +0530

    Day 10 Tutorial Practice

---
+++ training-session-tutorials/MVCExample/Models/Patient.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCExample.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Allergies { get; set; }
+    }
+}
