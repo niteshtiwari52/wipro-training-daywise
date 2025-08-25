f727cf50fd4c3043c8a70f5666b9c9c5ba7ed3c5
niteshtiwari <nitesh.tiwari.coox@gmail.com>
2025-08-25 20:50:36 +0530

    Day 31 Tutorial: Repo style + Dotnet Project using vs code cmd line

---
+++ day_31/Models/HRISModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Department Name is required")]
+        [StringLength(50, ErrorMessage = "Department Name can not exceed 50 characters")]
+        public string Name { get; set; }  
+    }
+
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employee Name is required")]
+        public int Name { get; set; }
+
+        [ForeignKey("Department")]
+        [Required(ErrorMessage = "DepartmentId is required")]
+
+        public int DepartmentId { get; set; }
+
+    }
+
+}
