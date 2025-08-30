c0ef826b3262135e363f6d035cec2efd9ab53946
niteshtiwari <nitesh.tiwari.coox@gmail.com>
2025-08-30 09:16:40 +0530

    Sample MVC Changes + practice

---
+++ MileStone3 Prep-test/Demos/SampleMVCProject1/SampleMVC1/Controllers/UserProfileController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SampleMVC1.ViewModels;
+
+namespace SampleMVC1.Controllers
+{
+
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserProfileController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            var roles = currentUser != null
+                ? await _userManager.GetRolesAsync(currentUser)
+                : new List<string>();
+
+            var model = new UserProfileViewModel
+            {
+                UserName = currentUser?.UserName,
+                Email = currentUser?.Email,
+                Roles = roles,
+                Message = roles.Contains("Admin")
+                            ? "Welcome to Admin Dashboard"
+                            : "Welcome to User Dashboard"
+            };
+
+            return View(model);
+        }
+    }
+}
